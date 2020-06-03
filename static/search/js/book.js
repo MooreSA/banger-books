396048c422a0662f505de18467701c64eb4526a4
Seamus Moore <seamusmoore@Seamus-MacBook-Air.local>
2020-06-02 22:03:27 -0300

    Fix API call to Goodreads

--- static/search/js/book.js
+++ static/search/js/book.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector("#test").innerHTML = "test";
-});
+
+    const isbn = document.getElementById('isbn').innerHTML;
+
+    fetch(`http://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/review_counts.json?isbns=${isbn}&key=gT9afGYzdoskLjqMgUmUg`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('average_rating').innerHTML = `Average rating: ${data.books[0].average_rating}`
+        document.getElementById('reviews_count').innerHTML = `Review Count ${data.books[0].reviews_count}`
+    })
+})
